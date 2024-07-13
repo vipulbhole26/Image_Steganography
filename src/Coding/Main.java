befa751bd51a38267f91deaf05849b60004fa8fa
vipulbhole26 <vipulbhole121@gmail.com>
2024-07-13 11:17:54 +0530

    Add files via upload

---
+++ src/Coding/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Coding;
+
+
+/**
+ *
+ * @author Vijay
+ */
+
+
+public class Main {
+    public static void main(String[] args){
+        MainMenu mm = new MainMenu();
+        mm.setVisible(true);
+        mm.setLocationRelativeTo(null);
+    }
+}
